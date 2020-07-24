371995ad0f96359c795fe6367d5821ed12919c27
dlsakbo <56906001+dlsakbo@users.noreply.github.com>
2020-07-24 15:02:20 +0530

    create login

--- Application/Application/Application/Form1.cs
+++ Application/Application/Application/Form1.cs
@@ -16,5 +16,23 @@ namespace Application
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String pass, user;
+            pass = textBox2.Text;
+            user = textBox1.Text;
+            if(user = "care" && pass = "admin")
+            {
+                Form2 mainform = new Form2();
+                mainForm.Visible = true;
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username Or Password")
+            }
+
+        }
     }
 }
