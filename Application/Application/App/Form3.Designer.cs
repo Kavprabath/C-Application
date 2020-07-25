47a69036610375ceaef57d177b1606bf1d348874
dlsakbo <56906001+dlsakbo@users.noreply.github.com>
2020-07-25 11:38:25 +0530

    add password txt

--- Application/Application/App/Form3.Designer.cs
+++ Application/Application/App/Form3.Designer.cs
@@ -37,9 +37,10 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(104, 239);
+            this.button1.Location = new System.Drawing.Point(156, 368);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(179, 41);
+            this.button1.Size = new System.Drawing.Size(268, 63);
             this.button1.TabIndex = 0;
             this.button1.Text = "AUTHENTICATE";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,10 +48,11 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(104, 159);
+            this.textBox1.Location = new System.Drawing.Point(156, 245);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.PasswordChar = '#';
-            this.textBox1.Size = new System.Drawing.Size(179, 20);
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(266, 26);
             this.textBox1.TabIndex = 1;
             // 
             // label1
@@ -58,23 +60,25 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(44, 118);
+            this.label1.Location = new System.Drawing.Point(66, 182);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(287, 24);
+            this.label1.Size = new System.Drawing.Size(444, 33);
             this.label1.TabIndex = 2;
             this.label1.Text = "ENTER ADMINISRTRATER KEY";
             // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(389, 334);
+            this.ClientSize = new System.Drawing.Size(584, 514);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form3";
             this.Text = "CARE DISTHIBUTION ADMIN";
             this.ResumeLayout(false);
