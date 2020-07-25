27a1dd94a1e229c467e350c9c03c7369ad151c5c
dlsakbo <56906001+dlsakbo@users.noreply.github.com>
2020-07-25 11:32:59 +0530

    ADD ADMIN PRIVILAGE

--- Application/Application/App/Form5.Designer.cs
+++ Application/Application/App/Form5.Designer.cs
@@ -28,27 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form5));
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(18, 27);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 127);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(145, 62);
+            this.button1.Size = new System.Drawing.Size(215, 50);
             this.button1.TabIndex = 0;
-            this.button1.Text = "NEW EMPLOYEE";
+            this.button1.Text = "ADD NEW EMPLOYEE";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(215, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "EMPLOYEE DETAILS";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(531, 432);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form5";
-            this.Text = "Form5";
+            this.Text = "ADMIN PRIVILAGES";
             this.ResumeLayout(false);
 
         }
@@ -56,5 +73,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
